1b49a34bb5a4bebd61486f06d247a2647a3a12fe
Simone Potenza <simone.potenza@gmail.com>
2017-12-05 22:39:04 +0100

    Folder restructuring

---
+++ src/App/App.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Gigsintown</h1>
+        <div>
+          <h2>Coming soon</h2>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
